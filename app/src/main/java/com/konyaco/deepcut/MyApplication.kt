8a6a9e201cb949f7bf4a487c733c82a48bfd3ab4
KonYaco <atwzj233@gmail.com>
2023-08-29 21:11:18 +0800

    Initial commit

---
+++ app/src/main/java/com/konyaco/deepcut/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.konyaco.deepcut
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+}
---
+++ app/src/main/java/com/konyaco/deepcut/viewmodel/AppViewModel.kt
@@ -0,0 +1,10 @@
+package com.konyaco.deepcut.viewmodel
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AppViewModel @Inject constructor() : ViewModel() {
+
+}
